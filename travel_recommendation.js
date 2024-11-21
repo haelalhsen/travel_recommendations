ac46edef8740896b817c9c61b06e61075f4620eb
haelalhsen <hael.alhsen@gmail.com>
2024-11-21 07:09:07 -0500

    build result

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,31 +1,62 @@
 const btnSearch = document.getElementById('btnSearch');
 const btnClear = document.getElementById('btnclear');
-btnSearch.aaddEventListener("click", searchRecomendations);
-btnClear.aaddEventListener("click", clearRecomendations);
 
-const patients = [];
-function addPatient() {
-        const name = document.getElementById("name").value;
-        const gender = document.querySelector('input[name="gender"]:checked');
-        const age = document.getElementById("age").value;
-        const condition = document.getElementById("condition").value;
+btnClear.addEventListener("click",()=>{
+    document.getElementById('keyword').value="";
+})
+btnSearch.addEventListener("click",()=>{
+    const input = document.getElementById('keyword').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
 
-        if (name && gender && age && condition) {
-          patients.push({ name, gender: gender.value, age, condition });
-          resetForm();
-          generateReport();
+    fetch('travel_recommendation_api.json')
+      .then(response => response.json())
+      .then(data => {
+        const countries=data.countries;
+        const beaches=data.beaches;
+        const temples=data.temples;
+        let recomedations=[];
+        debugger;
+        if(input==="country"){
+            countries.forEach(element => {
+                element.cities.forEach(city =>{
+                    recomedations.push(city);
+                })
+            });
+        }else if(input==="beach"){
+            recomedations=beaches;
+        }else if(input==="temple"){
+            recomedations=temples;
+        }else{
+            recomedations.concat (temples.find(item => item.name.toLowerCase() === input));
+            recomedations.concat ( beaches.find(item => item.name.toLowerCase() === input));
+            countries.forEach(element => {
+                element.cities.forEach(city =>{
+                    if(city===input)
+                    recomedations.push(city);
+                });
+            });
         }
-}
 
-function resetForm() {
-        document.getElementById("name").value = "";
-        document.querySelector('input[name="gender"]:checked').checked = false;
-        document.getElementById("age").value = "";
-        document.getElementById("condition").value = "";
-}
+        if (recomedations.length >0) {
+            console.log(recomedations);
+            recomedations.forEach(element=>{
+                // resultDiv.innerHTML += `<img src="${element.imageUrl}" alt="hjh">`;
+                resultDiv.innerHTML += `<h2>${element.name}</h2>`;
+            });
+        } else {
+          resultDiv.innerHTML = '';
+          console.log("hiiiii iam here");
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+})
 
 
-function searchCondition() {
+function searchRecomendations() {
     const input = document.getElementById('keyword').value.toLowerCase();
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
@@ -75,8 +106,8 @@ function searchCondition() {
 }
 
 function clearRecomendations(){
-    const input = document.getElementById('keyword').value.toLowerCase();
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
-    input.value="";
+    document.getElementById('keyword').value="";
+    // document.getElementById('result').innerHTML ="";
 }
+
+// btnSearch.aaddEventListener("click", searchRecomendations);
